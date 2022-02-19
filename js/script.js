a053bda78ae3ac331516d4e502023411beb7cb6e
Christoph <c.wagner.germany@gmail.com>
2022-02-19 17:58:20 +0100

    Popup styled

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let pokemonDetails = [];
+
 function init() {
     getPokemonAPIData();
 }
@@ -17,23 +19,43 @@ async function getSinglePokemonData(pokemonJSON) {
         const singlePokemonURL = pokemonJSON['results'][i]['url'];
         const response = await fetch(singlePokemonURL);
         const singlePokemonJSON = await response.json();
-        renderPokemonListCard(singlePokemonJSON);
+        renderPokemonListCard(i, singlePokemonJSON);
     }
 }
 
 
-function renderPokemonListCard(singlePokemonJSON) {
+function renderPokemonListCard(i, singlePokemonJSON) {
     const pokemonName = singlePokemonJSON['forms'][0]['name'];
     const pokemonImage = singlePokemonJSON['sprites']['other']['dream_world']['front_default'];
     const pokemonType = singlePokemonJSON['types'][0]['type']['name'];
 
-    renderPokemonListCardHTML(pokemonName, pokemonImage, pokemonType);
+    const pokemonAbility = singlePokemonJSON['abilities'][0]['ability']['name'];
+    const pokemonHeight = singlePokemonJSON['height'];
+    const pokemonWeight = singlePokemonJSON['weight'];
+
+    pushSinglePokemonDetails(pokemonName, pokemonImage, pokemonType, pokemonAbility, pokemonHeight, pokemonWeight);
+
+    renderPokemonListCardHTML(i, pokemonName, pokemonImage, pokemonType);
 }
 
 
-function renderPokemonListCardHTML(pokemonName, pokemonImage, pokemonType) {
+function pushSinglePokemonDetails(pokemonName, pokemonImage, pokemonType, pokemonAbility, pokemonHeight, pokemonWeight) {
+    const pokemon = {
+        'name': pokemonName,
+        'img': pokemonImage,
+        'type': pokemonType,
+        'ability': pokemonAbility,
+        'height': pokemonHeight,
+        'weight': pokemonWeight
+    }
+
+    pokemonDetails.push(pokemon);
+}
+
+
+function renderPokemonListCardHTML(i, pokemonName, pokemonImage, pokemonType) {
     document.getElementById("listPokemonCards").innerHTML += `
-        <div class="col-12 col-sm-6 col-md-4">
+        <div class="col-12 col-sm-6 col-md-4" onclick="openDetailView(${i})">
             <div class="card mb-4 list__card shadow-sm hvr-float-shadow">
                 <img id="listPokemonImage" class="list__pokemon__image"  src="${pokemonImage}" alt="${pokemonName}">
                 <div class="card-body">
@@ -46,17 +68,11 @@ function renderPokemonListCardHTML(pokemonName, pokemonImage, pokemonType) {
 }
 
 
-
-
-
-// document.getElementById("pop").addEventListener("click", function() {
-//     document.getElementsByClassName("detail__view__card")[0].classList.add("active");
-// });
-
-function openpop() {
+function openDetailView() {
     document.getElementsByClassName("popup")[0].classList.add("active");
 }
 
-function closepop() {
+
+function closeDetailView() {
     document.getElementsByClassName("popup")[0].classList.remove("active");
 }
